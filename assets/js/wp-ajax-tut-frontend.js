cceb0368cc1275b5bd12dcb380f3556105589894
EngageWP <mail@engagewp.com>
2015-12-29 01:04:03 -0500

    Minor tweaks

--- assets/js/wp-ajax-tut-frontend.js
+++ assets/js/wp-ajax-tut-frontend.js
@@ -6,6 +6,7 @@ jQuery(document).ready(function($) {
 	$('#wp-ajax-tut-logout').click( function() {
 
 		var $this = $(this),
+			button_text = $this.text(),
 			ajaxurl = wp_ajax_tut_frontend.ajaxurl;
 
 		//* Data to make available via the $_POST variable
@@ -14,14 +15,19 @@ jQuery(document).ready(function($) {
 			wp_ajax_tut_frontend_nonce: wp_ajax_tut_frontend.wp_ajax_tut_frontend_nonce
 		};
 
-		// Provide some feedback to let the user know they clicked the button
-		$this.text('Logging out...');
+		// Disable the button and provide some feedback to the user
+		$this.prop('disabled','disabled').text('Logging out...');
 
 		//* Process the AJAX POST request
 		$.post( ajaxurl, data, function(response) {
 
-			// Disable button, change its text, then fade it out after 2 seconds
-			$this.prop('disabled','disabled').text(response.message).delay(2000).fadeOut();
+			if ( response.status == 'success' ) {
+				// Show success message, then fade out the button after 2 seconds
+				$this.text(response.message).delay(2000).fadeOut();
+			} else {
+				// Re-enable the button and revert to original text
+				$this.removeProp('disabled').text('Logout failed.').delay(2000).text(button_text);
+			}
 		});
 
 		return false;
